7cfc460d5a984d0d1246df34e4b9b0f86b86e80b
LinhLe18398 <114391347+LinhLe18398@users.noreply.github.com>
2023-04-14 14:21:54 +0700

    four commit

--- src/shape/main.java
+++ src/shape/main.java
@@ -5,12 +5,22 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhap side 1: ");
-        double side1 = scanner.nextDouble();
-        System.out.println("Nhap side 2: ");
-        double side2 = scanner.nextDouble();
-        System.out.println("Nhap side 3: ");
-        double side3 = scanner.nextDouble();
+        double side1, side2, side3;
+
+        while (true) {
+            System.out.println("Nhap side 1: ");
+            side1 = scanner.nextDouble();
+            System.out.println("Nhap side 2: ");
+            side2 = scanner.nextDouble();
+            System.out.println("Nhap side 3: ");
+            side3 = scanner.nextDouble();
+            if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1) {
+                break;
+            } else {
+                System.out.println("day khong phai la tam giac");
+            }
+        }
+
         System.out.println("Nhap mau sac cua tam giac: ");
         String color = scanner.next();
 
