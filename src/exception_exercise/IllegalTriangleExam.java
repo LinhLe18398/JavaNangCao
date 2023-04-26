486869367c0ea6942067f4b981c0ff0c26cadc1d
LinhLe18398 <114391347+LinhLe18398@users.noreply.github.com>
2023-04-26 14:20:01 +0700

    five commit

---
+++ src/exception_exercise/IllegalTriangleExam.java
@@ -0,0 +1,33 @@
+package exception_exercise;
+
+import java.util.Scanner;
+
+public class IllegalTriangleException extends Exception {
+
+    public IllegalTriangleException(String message) {
+        super(message);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap canh thu nhat: ");
+        float a = scanner.nextFloat();
+        System.out.println("Nhap canh thu hai: ");
+        float b = scanner.nextFloat();
+        System.out.println("Nhap canh thu ba: ");
+        float c = scanner.nextFloat();
+        try {
+            if (a <= 0 || b <= 0 || c <= 0) {
+                throw new IllegalTriangleException("Cac canh phai lon hon 0");
+            }
+
+            if (a + b <= c || a + c <= b || b + c <= a) {
+                throw new IllegalTriangleException("Tong 2 canh phai lon hon canh con lai");
+            }
+
+        } catch (IllegalTriangleException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
---
+++ src/exception_lab/CalculationExample.java
@@ -0,0 +1,2 @@
+package exception_lab;public class CalculationExample {
+}
